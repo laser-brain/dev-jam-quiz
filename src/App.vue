a269426124c873ccad6bd2e17348104ab23b6036
Michael Zink <20622728+laser-brain@users.noreply.github.com>
2021-12-11 22:13:00 +0100

    display categories from api response

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-
+import CategorySelection from './components/CategorySelection.vue';
 </script>
 
 <template>
-
+  <CategorySelection />
 </template>
 
 <style>
---
+++ src/components/CategorySelection.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-for="category in categories" :key="category.id">
+    <label>{{ category.name }}</label>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, ref } from 'vue'
+import { loadCategories } from '../services/quiz-api'
+import { ITriviaCategory } from '../types/api-responses'
+
+export default defineComponent({
+  setup() {
+    const categories = ref<ITriviaCategory[]>([])
+
+    onMounted(async () => {
+      const apiCategories = await loadCategories();
+      categories.value = apiCategories;
+    });
+    return {
+      categories
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>

1e338a086cef0c87cd648b26634c3eb5dcabfd57
Michael Zink <20622728+laser-brain@users.noreply.github.com>
2021-12-14 11:45:38 +0100

    added score modifier based on difficulty

--- src/components/QuizSummary.vue
+++ src/components/QuizSummary.vue
@@ -1,15 +1,25 @@
 <template>
   <h3>Finished</h3>
-  <div>You have answered {{ correctAnswers }}/{{ totalQuestions }} questions correctly. This took you <span class="highlight">{{ duration }}</span></div><br />
-  <div>You <span :class="finishType">{{ finishType }}</span> this quiz with a final score of <span class="highlight">{{ score }}</span>.</div>
-  <div class="questions-summary">    
-  <h3>Answer summary</h3>
+  <div>
+    You have answered {{ correctAnswers }}/{{ totalQuestions }} questions
+    correctly. This took you <span class="highlight">{{ duration }}</span>
+  </div>
+  <br />
+  <div>
+    You <span :class="finishType">{{ finishType }}</span> this quiz with a final
+    score of <span class="highlight">{{ score }}</span
+    >.
+  </div>
+  <div class="questions-summary">
+    <h3>Answer summary</h3>
     <div v-for="question in questions" :key="question.question" class="spoiler">
-      <label v-html=" question.question" /><br/>Correct answer:&nbsp;
+      <label v-html="question.question" /><br />Correct answer:&nbsp;
       <span
         :style="{ color: question.answeredCorrectly ? 'green' : 'red' }"
         v-html="question.correct_answer"
       />
+      <br />Difficulty: {{ question.difficulty }}, Points awarded:
+      {{ getScore(question) * 10 }}
     </div>
   </div>
 </template>
@@ -17,6 +27,12 @@
 <script lang="ts">
 import { IQuizQuestionViewModel } from "@/types/viewModels";
 import { defineComponent, PropType } from "vue";
+
+enum Difficulty {
+  Easy = "easy",
+  Hard = "hard",
+  Medium = "medium",
+}
 
 const PASS_SCORE = 75;
 
@@ -28,55 +44,79 @@ export default defineComponent({
     },
     correctAnswers: {
       type: Number,
-      required: true
+      required: true,
     },
     totalQuestions: {
       type: Number,
-      required: true
+      required: true,
     },
     duration: {
       type: String,
     },
   },
   setup(props) {
-    const score = props.correctAnswers / props.totalQuestions * 100;
-    
+    function getScore(question: IQuizQuestionViewModel): number {
+      function scoreModificator(mod: Difficulty) {
+        switch (mod) {
+          case Difficulty.Easy:
+            return 1;
+          case Difficulty.Medium:
+            return 1.25;
+          case Difficulty.Hard:
+            return 1.5;
+          default:
+            return 0;
+        }
+      }
+      return question.answeredCorrectly
+        ? scoreModificator(question.difficulty)
+        : 0;
+    }
+
+    const score =
+      (props.questions
+        .map(getScore)
+        .reduce((score, finalScore) => finalScore + score) /
+        props.totalQuestions) *
+      100;
+
     const passed = score > PASS_SCORE;
-    const finishType =  passed ? 'passed' : 'failed';
+    const finishType = passed ? "passed" : "failed";
     return {
       score,
-      finishType
-    }
-  }
+      finishType,
+      getScore,
+    };
+  },
 });
 </script>
 
 <style lang="scss" scoped>
-  .questions-summary {
-    display: flex;
-    flex-direction: column;
-    justify-content: left;
-    align-items:flex-start;
-    width: 50vw;
-    margin-top: 1.5em;
-    @media screen and (orientation: portrait) {
-      width: 90vw;
-    }
+.questions-summary {
+  display: flex;
+  flex-direction: column;
+  justify-content: left;
+  align-items: flex-start;
+  width: 50vw;
+  margin-top: 1.5em;
+  @media screen and (orientation: portrait) {
+    width: 90vw;
   }
+}
 
 .highlight {
   color: white;
 }
 
 .passed {
-color: green;
+  color: green;
 }
 
 .failed {
   color: red;
 }
-  .spoiler {
-    margin-top: .5rem;
-    text-align: left;
-  }
+.spoiler {
+  margin-top: 0.5rem;
+  text-align: left;
+}
 </style>
